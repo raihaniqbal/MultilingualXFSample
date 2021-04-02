e3dd648a80d4b5db723e36c09ebf0e212f442a6d
Raihan Iqbal <raihan.iqbal@gmail.com>
2021-04-02 22:32:06 +1100

    * Added arabic translation * Added current time to main page

--- MultilingualXFSample/Views/MainPage.xaml.cs
+++ MultilingualXFSample/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using MultilingualXFSample.Resources;
 using MultilingualXFSample.Views;
 using Xamarin.Forms;
@@ -13,6 +14,7 @@ namespace MultilingualXFSample
         public MainPage()
         {
             InitializeComponent();
+            LocalizedDateTime.Text = $"Current Time: {DateTime.Now.ToShortTimeString()}";
         }
         async void Handle_Clicked(object sender, System.EventArgs e)
         {
